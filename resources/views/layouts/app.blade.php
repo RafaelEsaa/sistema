cc36ce1ae9cc0e1eae7690b9118bb9abe644aa52
REsaa-PC\Rafael Esaa <esaarafael@hotmail.com>
2017-02-13 17:25:56 -0400

    New View and Materialize css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,23 @@
-<html lang="es">
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-    @include('layouts.header')
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
 
 <body>
-    <section class="content">
-        @yield('content')
-    </section>
+@include('layouts.header')
+<section class="content">
+    @yield('content')
+</section>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="/js/materialize.min.js"></script>
 </body>
-
 </html>
