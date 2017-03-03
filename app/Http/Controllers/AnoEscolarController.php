635edc9647a97cfc4955e26bf024a9936d180d57
rafaelesaa <esaarafael@hotmail.com>
2017-03-02 22:49:12 -0400

    Controller and Model ano escolar

---
+++ app/Http/Controllers/AnoEscolarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AnoEscolar;
+use Illuminate\Http\Request;
+
+class AnoEscolarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AnoEscolar  $anoEscolar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AnoEscolar $anoEscolar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AnoEscolar  $anoEscolar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AnoEscolar $anoEscolar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AnoEscolar  $anoEscolar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AnoEscolar $anoEscolar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AnoEscolar  $anoEscolar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AnoEscolar $anoEscolar)
+    {
+        //
+    }
+}
